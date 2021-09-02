2c12bb5f3edf4d8507f557402ba20bf1fa0380da
Sergey Fedorenko <fedorenko_s_a@mail.ru>
2021-09-02 21:50:04 +0300

    feat: добавить загрузку блоков картинок из JS

--- script.js
+++ script.js
@@ -1,12 +1,72 @@
-let openPopupButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.edit-form');
-let closePopupButton = document.querySelector('.edit-form__close-icon');
-let title = document.querySelectorAll('.edit-form__subtitle')[0];
-let subtitle = document.querySelectorAll('.edit-form__subtitle')[1];
-let infoTitle = document.querySelector('.profile__title');
-let infoSubtitle = document.querySelector('.profile__subtitle');
-let submitButton = document.querySelector('.submit-button');
-let formElement = document.querySelector('.edit-form__field');
+const openPopupButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.edit-form');
+const closePopupButton = document.querySelector('.edit-form__close-icon');
+const title = document.querySelectorAll('.edit-form__subtitle')[0];
+const subtitle = document.querySelectorAll('.edit-form__subtitle')[1];
+const infoTitle = document.querySelector('.profile__title');
+const infoSubtitle = document.querySelector('.profile__subtitle');
+const submitButton = document.querySelector('.submit-button');
+const formElement = document.querySelector('.edit-form__field');
+
+//Добавление карточкек
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+const cardsElement = document.querySelector('.elements');
+const cardTemplate = document.querySelector('#card-template').content;
+
+const addCard = (card) => {
+    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+    cardElement.querySelector('.element__button-delite').content;
+    cardElement.querySelector('.element__image').src = card.link;
+    cardElement.querySelector('.element__image').alt = 'image';
+    cardElement.querySelector('.element__title').textContent = card.name;
+    cardElement.querySelector('.element__button').content;
+    cardElement.querySelector('.element__like').content;
+  
+    cardsElement.prepend(cardElement);
+}
+
+/*const postingCards = (event) => {
+    event.preventDefault();
+
+    addCard({
+        name: '',
+        link: '',
+    });
+
+    postingFormElement.reset();
+};
+
+postingFormElement.addEventListener('submit', postingFormHandler);*/
+
+initialCards.forEach((card) => {
+    addCard(card);
+});
 
 function togglePopup() {
     if (!popup.classList.contains('edit-form_is-openid')) {
